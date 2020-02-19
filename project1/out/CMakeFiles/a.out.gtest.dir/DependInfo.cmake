71d25e4d7a60aee4b273277f031891bd27551446
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2020-02-19 11:48:10 -0800

    Add files via upload

---
+++ project1/out/CMakeFiles/a.out.gtest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/compsci260p/projects/proj1/app/proj1.cpp" "/home/compsci260p/projects/proj1/out/CMakeFiles/a.out.gtest.dir/app/proj1.cpp.o"
+  "/home/compsci260p/projects/proj1/gtest/gtestmain.cpp" "/home/compsci260p/projects/proj1/out/CMakeFiles/a.out.gtest.dir/gtest/gtestmain.cpp.o"
+  "/home/compsci260p/projects/proj1/gtest/proj1tests.cpp" "/home/compsci260p/projects/proj1/out/CMakeFiles/a.out.gtest.dir/gtest/proj1tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../app"
+  "../gtest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
